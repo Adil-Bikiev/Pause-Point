b61f7e43ab319ca5601a2a5b1c76ca39e89cd4e5
EMMMABK <adilbikiev2@gmail.com>
2025-06-13 23:34:26 +0600

    add Change to DeepSeek

--- server.cjs
+++ server.cjs
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const app = express();
 const PORT = process.env.PORT || 4000;
